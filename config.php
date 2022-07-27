de690c9c1ea51bc43117f0da85292c96f39533c4
sukumar <tensiletechofficial@gmail.com>
2022-07-27 23:13:09 +0530

    adding test bed

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php
 
-$dbconnect= mysqli_connect('localhost:3308','root','','kaiwa_internal');
-$baseurl ="http://localhost:8080/diy/kaiwa-landing.php/";
-
-
+$dbconnect= mysqli_connect('localhost:3306','root','','cogno_db');
 
 ?>
---
+++ test_bed.php
@@ -0,0 +1,10 @@
+<?php 
+include 'config.php';
+if($dbconnect){
+  echo " connected";
+}
+else{
+  echo " not connected";
+}
+
+?>
